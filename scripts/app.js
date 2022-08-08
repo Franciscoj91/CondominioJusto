e1faf77bb004804ba5dffb511adc96821e18593b
Francisco Marin <franjmarin1991@gmail.com>
2022-08-08 12:15:37 -0400

    navbar active class

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,19 @@
+//navbar
+let navLinks = document.getElementsByClassName('nav-link');
+console.log(navLinks);
+
+navLinks = Array.from(navLinks);
+
+navLinks.forEach(element => {
+    element.addEventListener("click", function() {
+        let currentEvent = document.getElementsByClassName('active');
+        currentEvent[0].className = currentEvent[0].className.replace('active', '');
+        element.className += ' active';   
+    })
+});  
+    
+    
+//carousel
 const myCarouselElement = document.querySelector('#carousel');
 const carousel = new bootstrap.Carousel(myCarouselElement, {
     interval: 5000,
